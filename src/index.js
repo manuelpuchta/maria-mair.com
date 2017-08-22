97e4599e1a47fe3ad7d7c94ab228611af1c6d8bb
manuelpuchta <manuel.puchta@gmail.com>
2017-08-22 23:33:50 +0200

    Init src components

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import './index.scss';
+
+// const dom = document.getElementById('app');
+
+class Index {
+  constructor() {
+    console.log('hi from Index class.');
+  }
+}
+
+const app = new Index();
