fa5d87c1501c70074bde53d88bb0f7c899fdfb3f
manuelpuchta <manuel.puchta@gmail.com>
2017-09-16 15:44:59 +0200

    Fix and update local-setup bash script

--- config/provision/shell/local-setup.sh
+++ config/provision/shell/local-setup.sh
@@ -1,24 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-function init_localCraftcmsSetup {
+# copy craftcms raw installation templates directory to _templates directory
+if [ -d /vagrant/craft/templates ] && [ ! -d /vagrant/craft/_templates ]; then
+	echo -e "Copy craft/templates to craft/_templates."
+	cp -rf /vagrant/craft/templates /vagrant/craft/_templates
+fi
 
-	# copy craftcms templates directory to _templates directory
-	if [[ -d /vagrant/craft/templates && ! -d /vagrant/craft/_templates ]]
-	then
-		echo -e "Copy craft/templates to craft/_templates."
-		cp -rf /vagrant/craft/templates /vagrant/craft/_templates
-	fi
+# remove raw craftcms installation templates directory
+if [ ! -L /vagrant/craft/templates ] && [ -d /vagrant/craft/templates ]; then
+	echo -e "Remove craft/templates directory."
+	rm -rf /vagrant/craft/templates
+fi
 
-	# delete craftcms templates directory and link src templates folder to craft directory
-	if ! [[ -L /vagrant/craft/templates && -d /vagrant/craft/templates ]]
-	then
-		echo -e "Remove craft/templates directory."
-		rm -rf /vagrant/craft/templates
-		echo -e "Link src/templates to craft directory."
-		ln -s /vagrant/src/templates /vagrant/craft
-	fi
-}
-
-init_localCraftcmsSetup
+# create a symbolic link of src/templates to craft directory
+if [ ! -L /vagrant/craft/templates ]; then
+	echo -e "Link src/templates to craft directory."
+	ln -s /vagrant/src/templates /vagrant/craft
+fi
 
 echo -e "\033[33;35mReady at http://development.maria-mair.com/"
