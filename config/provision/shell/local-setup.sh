8b1812dea95567b59d1f632bb777ac5cc1bf3c61
manuelpuchta <manuel.puchta@gmail.com>
2017-09-18 22:41:13 +0200

    Refactor local setup script

--- config/provision/shell/local-setup.sh
+++ config/provision/shell/local-setup.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+CRAFT_TEMPLATES=/vagrant/craft/templates
+SRC_TEMPLATES=/vagrant/src/templates
+
 # copy craftcms raw installation templates directory to _templates directory
-if [ -d /vagrant/craft/templates ] && [ ! -d /vagrant/craft/_templates ]; then
+if [ -d $CRAFT_TEMPLATES ] && [ ! -d /vagrant/craft/_templates ]; then
 	echo -e "Copy craft/templates to craft/_templates."
-	cp -rf /vagrant/craft/templates /vagrant/craft/_templates
+	cp -rf $CRAFT_TEMPLATES /vagrant/craft/_templates/
 fi
 
 # remove raw craftcms installation templates directory
-if [ ! -L /vagrant/craft/templates ] && [ -d /vagrant/craft/templates ]; then
+if [ ! -L $CRAFT_TEMPLATES ] && [ -d $CRAFT_TEMPLATES ]; then
 	echo -e "Remove craft/templates directory."
-	rm -rf /vagrant/craft/templates
+	rm -rf $CRAFT_TEMPLATES
 fi
 
 # create a symbolic link of src/templates to craft directory
-if [ ! -L /vagrant/craft/templates ]; then
+if [ ! -L $CRAFT_TEMPLATES ]; then
 	echo -e "Link src/templates to craft directory."
-	ln -s /vagrant/src/templates /vagrant/craft
+	ln -s /vagrant/src/templates /vagrant/craft/
 fi
 
 echo -e "\033[33;35mReady at http://development.maria-mair.com/"
